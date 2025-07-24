5968c3cb13023db44bc8aae9c6c7c4d50106f54f
Thyago Barbosa Afonso <thyago.afonso@unievangelica.edu.br>
2025-07-24 12:35:29 -0300

    submit.js

--- api/submit.js
+++ api/submit.js
@@ -6,18 +6,24 @@ export default async function handler(req, res) {
   try {
     const googleScriptUrl = 'https://script.google.com/macros/s/AKfycbzYCcjyjwqAZfCzn6ZMFjqN-bP7YgPCmC_8NGmn50qb7ZQ69SecW7VSpdgBtGCmHNuHPA/exec';
 
+    const formData = new URLSearchParams({
+      name: req.body.name,
+      email: req.body.email,
+      numero: req.body.numero
+    });
+
     const response = await fetch(googleScriptUrl, {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/json'
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: JSON.stringify(req.body)
+      body: formData.toString(),
     });
 
     const result = await response.text();
-    res.status(200).json({ message: 'Enviado com sucesso', result });
+    res.status(200).json({ message: 'Dados enviados com sucesso', result });
   } catch (error) {
-    console.error('Erro ao enviar:', error);
+    console.error('Erro ao enviar dados:', error);
     res.status(500).json({ message: 'Erro ao enviar dados', error: error.message });
   }
 }
